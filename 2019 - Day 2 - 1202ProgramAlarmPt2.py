4eba4dd02c7bab85de7cc7a8c50542067534b559
Anthony Nguyen <anthonyngu2@gmail.com>
2019-12-02 20:37:07 -0500

    Added return statement

--- 2019 - Day 2 - 1202ProgramAlarmPt2.py
+++ 2019 - Day 2 - 1202ProgramAlarmPt2.py
@@ -1,12 +1,14 @@
 ## store file into variable
 intCodeOriginal = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,2,6,23,27,1,6,27,31,2,31,9,35,1,35,6,39,1,10,39,43,2,9,43,47,1,5,47,51,2,51,6,55,1,5,55,59,2,13,59,63,1,63,5,67,2,67,13,71,1,71,9,75,1,75,6,79,2,79,6,83,1,83,5,87,2,87,9,91,2,9,91,95,1,5,95,99,2,99,13,103,1,103,5,107,1,2,107,111,1,111,5,0,99,2,14,0,0"
 intCodeOriginal = [int(x) for x in intCodeOriginal.split(',')]
+
 finalIntCode = []
 
 def OpCodeProgram(x,y,intCode):
-    startPosition = 0
     intCode[1] = x
     intCode[2] = y
+    startPosition = 0
+
     for code in range(0, len(intCode),4) :
         code = intCode[startPosition]
         if intCode[0] == 19690720:
@@ -17,26 +19,25 @@ def OpCodeProgram(x,y,intCode):
             factor2Position = intCode[startPosition + 2]
             factor3Position = intCode[startPosition + 3]
             intCode[factor3Position] = intCode[factor1Position] + intCode[factor2Position]
-            print(intCode)
-
             startPosition += 4
         if code == 2:
             factor1Position = intCode[startPosition + 1]
             factor2Position = intCode[startPosition + 2]
             factor3Position = intCode[startPosition + 3]
             intCode[factor3Position] = intCode[factor1Position] * intCode[factor2Position]
-            print(intCode)
-
             startPosition += 4
         if code == 99:
+            return intCode
             break
         else:
             continue
-
+        
+    return intCode
         
 for x in range(100):
     for y in range (100):
-        finalIntCode = OpCodeProgram(x,y,intCodeOriginal)
+        intCodeArg = list(intCodeOriginal)
+        finalIntCode = OpCodeProgram(x,y,intCodeArg)
 
 print(finalIntCode)
 ##noun = finalIntCode[1]
