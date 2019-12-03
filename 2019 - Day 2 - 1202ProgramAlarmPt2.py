3eeaf84599abb6b5bf5098c6c41ba9115db2f95c
Anthony Nguyen <anthonyngu2@gmail.com>
2019-12-02 19:09:28 -0500

    added day two part 1

---
+++ 2019 - Day 2 - 1202ProgramAlarmPt2.py
@@ -0,0 +1,45 @@
+## store file into variable
+intCodeOriginal = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,2,6,23,27,1,6,27,31,2,31,9,35,1,35,6,39,1,10,39,43,2,9,43,47,1,5,47,51,2,51,6,55,1,5,55,59,2,13,59,63,1,63,5,67,2,67,13,71,1,71,9,75,1,75,6,79,2,79,6,83,1,83,5,87,2,87,9,91,2,9,91,95,1,5,95,99,2,99,13,103,1,103,5,107,1,2,107,111,1,111,5,0,99,2,14,0,0"
+intCodeOriginal = [int(x) for x in intCodeOriginal.split(',')]
+finalIntCode = []
+
+def OpCodeProgram(x,y,intCode):
+    startPosition = 0
+    intCode[1] = x
+    intCode[2] = y
+    for code in range(0, len(intCode),4) :
+        print(intCode)
+        code = intCode[startPosition]
+        if intCode[0] == 19690720:
+            finalIntCode = list(intCode)
+        if code == 1:
+            factor1Position = intCode[startPosition + 1]
+            factor2Position = intCode[startPosition + 2]
+            factor3Position = intCode[startPosition + 3]
+            intCode[factor3Position] = intCode[factor1Position] + intCode[factor2Position]
+            startPosition += 4
+        elif code == 2:
+            factor1Position = intCode[startPosition + 1]
+            factor2Position = intCode[startPosition + 2]
+            factor3Position = intCode[startPosition + 3]
+            intCode[factor3Position] = intCode[factor1Position] * intCode[factor2Position]
+            startPosition += 4
+        elif code == 99:
+            startPosition += 1
+            break
+        else:
+            continue
+    return finalIntCode
+        
+for x in range(0,100,1):
+    for y in range (0,100,1):
+        print(OpCodeProgram(x,y,intCodeOriginal))
+        
+##noun = finalIntCode[1]
+##verb = finalIntCode[2]
+##product = 1000 * noun + verb
+##print(product)
+            
+            
+
+
