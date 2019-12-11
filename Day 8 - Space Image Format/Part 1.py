aec28ef398abe1564b52724a2511b1d0d159eae5
Anthony Nguyen <anthonyngu2@gmail.com>
2019-12-11 12:27:52 -0500

    populating log

--- Day 8 - Space Image Format/Part 1.py
+++ Day 8 - Space Image Format/Part 1.py
@@ -2,42 +2,45 @@ import math
 with open ('/Users/anthonynguyen/Desktop/Advent-Of-Code-2019/Day 8 - Space Image Format/Input.txt') as file:
     Image = file.read()
 
-def decoder(file, width, height):
-    complete_log = []
+def create_sub_list(file, width, height):
     layer_size = width * height
-    zero_count = 0
-    one_count = 0
-    two_count = 0
-    log = {}
     list_of_layers = []
     
     for start in range(0, len(file), layer_size):
         end = start + layer_size
         sub_list = file[start:end]
         list_of_layers.append(sub_list)
-
-    for layer_number, layer_set in enumerate(list_of_layers):
-        for digit in layer_set:
-            if digit == '0':
-                zero_count += 1
-            if digit == '1':
-                one_count += 1
-            if digit == '2':
-                two_count += 1
-            if zero_count + one_count + two_count == layer_size:
-                log['layer %s' % layer_number] =  layer_number 
-                log['zero count in layer %s' % layer_number] = zero_count
-                log['one count layer %s' % layer_number] = one_count
-                log['two count layer %s' % layer_number] = two_count
-                layer_number += 1
-                zero_count = one_count = two_count = 0
-                complete_log.append(log)
-                break
-    
-    return complete_log            
-    
+        
+    return list_of_layers
 
 Width = 25
 Height = 6
-image_log = decoder(Image, Width, Height)
-print(image_log)
+layered_file = create_sub_list(Image, Width, Height)
+
+def decoder(number_of_layer, layer):
+    zero_count = 0
+    one_count = 0
+    two_count = 0
+    log = {}
+
+    for digit in layer:
+        if digit == '0':
+            zero_count += 1
+        if digit == '1':
+            one_count += 1
+        if digit == '2':
+            two_count += 1
+        if zero_count + one_count + two_count == len(layer):
+            log['layer %s' % number_of_layer] =  number_of_layer 
+            log['zero count in layer %s' % number_of_layer] = zero_count
+            log['one count layer %s' % number_of_layer] = one_count
+            log['two count layer %s' % number_of_layer] = two_count
+    
+    return log            
+
+full_log = []
+for layer_number, layer in enumerate(layered_file):
+    layer_log = decoder(layer_number, layer)
+    full_log.append(layer_log)
+
+print(full_log)
