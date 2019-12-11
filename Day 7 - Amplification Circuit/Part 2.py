cdce59f617880e3d5d0b2369dc0b3d87c96e1ee7
Anthony Nguyen <anthonyngu2@gmail.com>
2019-12-10 20:01:01 -0500

    accessing full logs in function

--- Day 7 - Amplification Circuit/Part 2.py
+++ Day 7 - Amplification Circuit/Part 2.py
@@ -4,7 +4,8 @@ from collections import Counter
 int_code_list = '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0'
 #int_code_list = '3,8,1001,8,10,8,105,1,0,0,21,34,51,64,81,102,183,264,345,426,99999,3,9,102,2,9,9,1001,9,4,9,4,9,99,3,9,101,4,9,9,102,5,9,9,1001,9,2,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,4,9,99,3,9,102,3,9,9,101,3,9,9,1002,9,4,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,1002,9,5,9,101,3,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99'
 
-def test_diagnostic_program(sequence_value, output_value):
+def test_diagnostic_program(sequence_value, output_value, final_output_log):
+    final_log = final_output_log
     int_code = [int(x) for x in int_code_list.split(',')]
     sequence = sequence_value
     test_results = 0
@@ -113,26 +114,24 @@ def test_diagnostic_program(sequence_value, output_value):
 
 ##sequence_list = generate_list()
         
-def initiate_thrusters(sequence, sequence_output, counter, output_log):
+def initiate_thrusters(sequence, sequence_output, counter, final_log):
     if counter == 5:
-        return sequence_output
+        return final_log[counter-1]
     else:
         counter += 1
-        log = test_diagnostic_program(sequence[counter-1], sequence_output)
-        output_log.append(log)
+        log = test_diagnostic_program(sequence[counter-1], sequence_output, final_log)
+        final_log.append(log)
         sequence_output = log['test results']
-        return initiate_thrusters(sequence, sequence_output, counter, output_log)
+        return initiate_thrusters(sequence, sequence_output, counter, final_log)
 
 def determine_thruster_signal():
     thruster_signals = []
-    final_output_log = []
     sequence = [1,0,4,3,2]
-    
     #for sequence in sequence_list:
-    max_thruster_signal = initiate_thrusters(sequence, 0, 0, final_output_log)
+    max_thruster_signal = initiate_thrusters(sequence, 0, 0, thruster_signals)
     thruster_signals.append(max_thruster_signal)
     return thruster_signals
 
 
-print(determine_thruster_signal())
+print(determine_thruster_signal()[-1]['test results'])
 
