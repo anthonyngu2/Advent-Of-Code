705e084f2cd574a94d79d1f9902e2f93fdb07029
Anthony Nguyen <anthonyngu2@gmail.com>
2019-12-19 11:45:56 -0500

    passed 1 test

--- Day 9 - Sensor Boost /Part 1.py
+++ Day 9 - Sensor Boost /Part 1.py
@@ -10,95 +10,95 @@ def test_diagnostic_program(int_code, input_value):
     position = 0
 
     log = {'int code': int_code, 'test results': test_results}
-        while position in range(len(int_code)):
+    while position in range(len(int_code)):
 
-            mode_opcode = str(int_code[position]).zfill(5)  # add zeros to full opcode
-            mode_opcode_list = list(mode_opcode)  # make list
-            # convert list strings to ints
-            opcode_modes = [int(x) for x in mode_opcode_list]
-            opcode = str(opcode_modes[3]) + str(opcode_modes[4])
-            opcode = int(opcode)
-            params_mode = opcode_modes[:3]
-            param_pairs = list(enumerate(params_mode[::-1], start=1))
-            param_values = []
+        mode_opcode = str(int_code[position]).zfill(5)  # add zeros to full opcode
+        mode_opcode_list = list(mode_opcode)  # make list
+        # convert list strings to ints
+        opcode_modes = [int(x) for x in mode_opcode_list]
+        opcode = str(opcode_modes[3]) + str(opcode_modes[4])
+        opcode = int(opcode)
+        params_mode = opcode_modes[:3]
+        param_pairs = list(enumerate(params_mode[::-1], start=1))
+        param_values = []
 
-            for param_position, mode in param_pairs:
-                if opcode in (3, 4, 9, 99):
-                    break
-                if param_position == 3:
-                    param_value = int_code[position + param_position]
-                    param_values.append(param_value)
-                elif mode == 0:  # position mode
+        for param_position, mode in param_pairs:
+            if opcode in (3, 4, 9, 99):
+                break
+            if param_position == 3:
+                param_value = int_code[position + param_position]
+                param_values.append(param_value)
+            elif mode == 0:  # position mode
+                param_value = int_code[int_code[position + param_position]]
+                param_values.append(param_value)
+            elif mode == 1:  # immediate mode
+                param_value = int_code[position + param_position]
+                param_values.append(param_value)
+            elif mode == 2:  # relative mode
+                if relative_base == 0:
                     param_value = int_code[int_code[position + param_position]]
                     param_values.append(param_value)
-                elif mode == 1:  # immediate mode
-                    param_value = int_code[position + param_position]
+                elif relative_base != 0:
+                    param_value = int_code[relative_base + int_code[position + param_position]]
                     param_values.append(param_value)
-                elif mode == 2:  # relative mode
-                    if relative_base == 0:
-                        param_value = int_code[int_code[position + param_position]]
-                        param_values.append(param_value)
-                    elif relative_base != 0:
-                        param_value = int_code[relative_base + int_code[position + param_position]]
-                        param_values.append(param_value)
-                    else:
-                        break
-
-            if opcode == 1:
-                int_code[param_values[2]] = param_values[0] + param_values[1]
-                position += 4
-
-            elif opcode == 2:
-                int_code[param_values[2]] = param_values[0] * param_values[1]
-                position += 4
-
-            elif opcode == 3:
-                int_code[int_code[position + 1]] = input_value
-                position += 2
-
-            elif opcode == 4:
-                test_results = int_code[int_code[position + 1]]
-                position += 2
-
-            elif opcode == 5:
-                if param_values[0] != 0:
-                    position = param_values[1]
                 else:
-                    position += 3
+                    break
 
-            elif opcode == 6:
-                if param_values[0] == 0:
-                    position = param_values[1]
-                else:
-                    position += 3
+        if opcode == 1:
+            int_code[param_values[2]] = param_values[0] + param_values[1]
+            position += 4
 
-            elif opcode == 7:
-                position += 4
-                if param_values[0] < param_values[1]:
-                    int_code[param_values[2]] = 1
-                else:
-                    int_code[param_values[2]] = 0
+        elif opcode == 2:
+            int_code[param_values[2]] = param_values[0] * param_values[1]
+            position += 4
 
-            elif opcode == 8:
-                position += 4
-                if param_values[0] == param_values[1]:
-                    int_code[param_values[2]] = 1
-                else:
-                    int_code[param_values[2]] = 0
+        elif opcode == 3:
+            int_code[int_code[position + 1]] = input_value
+            position += 2
 
-            elif opcode == 9:
-                position += 2
-                relative_base = relative_base + int_code[position + 1]
-            elif opcode == 99:
-                log['halted'] = True
-                break
+        elif opcode == 4:
+            test_results = int_code[position + 1] # removed outer int_code[] but may need to add back in
+            position += 2
 
+        elif opcode == 5:
+            if param_values[0] != 0:
+                position = param_values[1]
             else:
-                continue
+                position += 3
 
-        log['int code'] = int_code
-        log['test results'] = test_results
-        return log
+        elif opcode == 6:
+            if param_values[0] == 0:
+                position = param_values[1]
+            else:
+                position += 3
+
+        elif opcode == 7:
+            position += 4
+            if param_values[0] < param_values[1]:
+                int_code[param_values[2]] = 1
+            else:
+                int_code[param_values[2]] = 0
+
+        elif opcode == 8:
+            position += 4
+            if param_values[0] == param_values[1]:
+                int_code[param_values[2]] = 1
+            else:
+                int_code[param_values[2]] = 0
+
+        elif opcode == 9:
+            position += 2
+            relative_base = relative_base + int_code[position + 1]
+        elif opcode == 99:
+            log['halted'] = True
+            break
+
+        else:
+            continue
+
+    log['int code'] = int_code
+    log['test results'] = test_results
+    return log
 
 
 output = test_diagnostic_program(int_code_list, 0)
