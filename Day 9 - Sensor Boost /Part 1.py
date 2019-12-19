280c20d0a1ca59305fe9902a8b5479ea0422e23e
Anthony Nguyen <anthonyngu2@gmail.com>
2019-12-18 21:26:55 -0500

    configured op code 9 and mode 2

--- Day 9 - Sensor Boost /Part 1.py
+++ Day 9 - Sensor Boost /Part 1.py
@@ -1,9 +1,7 @@
 import pprint
 from collections import Counter
 import sys
-sys.setrecursionlimit(10**7)
-int_code_string = '3,8,1001,8,10,8,105,1,0,0,21,34,51,64,81,102,183,264,345,426,99999,3,9,102,2,9,9,1001,9,4,9,4,9,99,3,9,101,4,9,9,102,5,9,9,1001,9,2,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,4,9,99,3,9,102,3,9,9,101,3,9,9,1002,9,4,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,1002,9,5,9,101,3,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99'
-#int_code_string = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
+
 int_code_original = [int(x) for x in int_code_string.split(',')]
 
 def test_diagnostic_program(int_code_original, sequence_value, output_value, final_output_log, log_position):
@@ -40,8 +38,6 @@ def test_diagnostic_program(int_code_original, sequence_value, output_value, fin
 
         
         for param_position,mode in param_pairs:
-            if opcode in (3,4,99): #maybe remove 
-                break
             if param_position == 3:
                 param_value = int_code[position + param_position]
                 param_values.append(param_value)
@@ -52,13 +48,14 @@ def test_diagnostic_program(int_code_original, sequence_value, output_value, fin
                 param_value = int_code[position + param_position]
                 param_values.append(param_value)
             elif mode == 2: #relative mode
-                if relative_base == 0:
+                if relative_base == 0 and param_position == 0:
                     param_value = int_code[int_code[position + param_position]]
                     param_values.append(param_value)
-                else:
-                    relative_base = relative_base + int_code[position + param_position]
-                    param_value = int_code[int_code[relative_mode_param]]
+                elif relative_base != 0 and param_position == 0:
+                    param_value = int_code[relative_base + int_code[position + param_position]]
                     param_values.append(param_value)
+                else:
+                    break
         if opcode == 1:
             int_code[param_values[2]] = param_values[0] + param_values[1]
             position += 4
@@ -85,7 +82,7 @@ def test_diagnostic_program(int_code_original, sequence_value, output_value, fin
             position += 2
             
         elif opcode == 4:
-            test_results = int_code[int_code[position + 1]]
+            test_results = param_value[0]
             position += 2
             break
             
@@ -114,7 +111,9 @@ def test_diagnostic_program(int_code_original, sequence_value, output_value, fin
                 int_code[param_values[2]] = 1
             else:
                 int_code[param_values[2]] = 0
-                
+        elif opcode == 9:
+            position += 2
+            relative_base = relative_base + param_values[0]
         elif opcode == 99:
             log['halted'] = True
             break
